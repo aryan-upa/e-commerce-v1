23a69494d8ce4580095b1eee055d8089e1497727
Aryan Upadhyay <aryan.offc@gmail.com>
2023-04-06 22:56:48 +0530

    added basic error view and handling

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,26 +3,41 @@ const router = express.Router();
 const Product = require('../models/Product');
 
 router.get('/', async (req, res) => { // for /products/
-    const products = await Product.prod.find({});
-    res.render('./products/product', {products});
+    try {
+        const products = await Product.prod.find({});
+        res.render('./products/product', {products});
+    } catch (err) {
+        res.status(500);
+        res.render('error', {err});
+    }
 });
 
 router.get('/show', (req, res) => { // for /products/show
-    res.send('showing products!');
-    res.end();
+    try{
+        res.send('showing products!');
+        res.end();
+    } catch (err) {
+        res.status(500);
+        res.render('error', {err});
+    }
 });
 
 router.post('/', async (req, res) => {
-    const {name, img, desc, price} = req.body;
+    try {
+        const {name, img, desc, price} = req.body;
 
-    await Product.prod.insertMany([{
-        name: name,
-        price: price,
-        desc: desc,
-        img: img
-    }]);
+        await Product.prod.insertMany([{
+            name: name,
+            price: price,
+            desc: desc,
+            img: img
+        }]);
 
-    res.redirect('/products');
+        res.redirect('/products');
+    } catch (err) {
+        res.status(500);
+        res.render('error', {err});
+    }
 });
 
 router.get('/new', (req, res) => {
@@ -30,35 +45,53 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:productID', async (req, res) => {
-    const {productID} = req.params;
-    const product = await Product.prod.findById(productID).populate('review');
-    res.render('./products/show', {product});
+    try {
+        const {productID} = req.params;
+        const product = await Product.prod.findById(productID).populate('review');
+        res.render('./products/show', {product});
+    } catch (err) {
+        res.status(500);
+        res.render('error', {err});
+    }
 });
 
 router.get('/:productID/edit', async (req, res) => {
-    const {productID} = req.params;
-    const product = await Product.prod.findById(productID);
-    res.render('./products/edit', {product});
+    try {
+        const {productID} = req.params;
+        const product = await Product.prod.findById(productID);
+        res.render('./products/edit', {product});
+    } catch (err) {
+        res.status(500);
+        res.render('error', {err});
+    }
 });
 
 router.patch ('/:productID', async (req, res) => {
-    const {productID} = req.params;
-    const {name, img, price, desc} = req.body;
-    // console.log(req.body);
+    try {
+        const {productID} = req.params;
+        const {name, img, price, desc} = req.body;
 
+        const product = await Product.prod.findByIdAndUpdate(productID, {name, img, price, desc});
+        console.log(product);
 
-    const product = await Product.prod.findByIdAndUpdate(productID, {name, img, price, desc});
-    console.log(product);
-
-    res.redirect('/products/'+productID);
+        res.redirect('/products/'+productID);
+    } catch (err) {
+        res.status(500);
+        res.render('error', {err});
+    }
 });
 
 router.delete('/:productID', async (req, res) => {
-    const {productID} = req.params;
-    const product = await Product.prod.findById({_id: productID});
+    try {
+        const {productID} = req.params;
+        const product = await Product.prod.findById({_id: productID});
 
-    await Product.prod.findByIdAndDelete({_id: productID});
-    res.redirect('/products');
+        await Product.prod.findByIdAndDelete({_id: productID});
+        res.redirect('/products');
+    } catch (err) {
+        res.status(500);
+        res.render('error', {err});
+    }
 })
 
 module.exports = router;
